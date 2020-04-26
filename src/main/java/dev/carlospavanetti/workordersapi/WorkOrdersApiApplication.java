03e3cd363bc5d91b205490bcf71731ed46a71ff1
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-26 01:33:09 -0300

    Init spring-boot repo

---
+++ src/main/java/dev/carlospavanetti/workordersapi/WorkOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package dev.carlospavanetti.workordersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrdersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carlospavanetti/workordersapi/WorkOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlospavanetti.workordersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
