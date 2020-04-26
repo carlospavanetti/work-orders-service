3eedf7c055a2ae211d7d388fb793ceaea42bea64
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-26 15:16:19 -0300

    Update controller to use JPA repository

--- src/main/java/dev/carlospavanetti/workordersapi/api/controller/CustomerController.java
+++ src/main/java/dev/carlospavanetti/workordersapi/api/controller/CustomerController.java
@@ -2,21 +2,20 @@ package dev.carlospavanetti.workordersapi.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.carlospavanetti.workordersapi.domain.model.Customer;
+import dev.carlospavanetti.workordersapi.domain.model.CustomersRepository;
 
 @RestController
 public class CustomerController {
-  @PersistenceContext
-  private EntityManager manager;
+  @Autowired
+  private CustomersRepository repository;
 
   @GetMapping("/customers")
   public List<Customer> list() {
-    return manager.createQuery("from Customers", Customer.class).getResultList();
+    return repository.findAll();
   }
 }
---
+++ src/main/java/dev/carlospavanetti/workordersapi/domain/model/CustomersRepository.java
@@ -0,0 +1,9 @@
+package dev.carlospavanetti.workordersapi.domain.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customer, Long> {
+
+}
