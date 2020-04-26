4495504c7736084ff92a21c6c5dfc569533a8a47
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-26 16:03:28 -0300

    Complete customers controller actions

--- src/main/java/dev/carlospavanetti/workordersapi/api/controller/CustomerController.java
+++ src/main/java/dev/carlospavanetti/workordersapi/api/controller/CustomerController.java
@@ -1,21 +1,66 @@
 package dev.carlospavanetti.workordersapi.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.carlospavanetti.workordersapi.domain.model.Customer;
 import dev.carlospavanetti.workordersapi.domain.model.CustomersRepository;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
   @Autowired
   private CustomersRepository repository;
 
-  @GetMapping("/customers")
+  @GetMapping
   public List<Customer> list() {
     return repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Customer> show(@PathVariable Long id) {
+    Optional<Customer> customer = repository.findById(id);
+
+    return customer.isPresent() ? ResponseEntity.ok(customer.get()) : ResponseEntity.notFound().build();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Customer create(@RequestBody Customer customer) {
+    return repository.save(customer);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Customer> update(@PathVariable Long id, @RequestBody Customer customer) {
+    if (!repository.existsById(id)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    customer.setId(id);
+    repository.save(customer);
+    return ResponseEntity.ok(customer);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    if (!repository.existsById(id)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    repository.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/dev/carlospavanetti/workordersapi/domain/model/Customer.java
+++ src/main/java/dev/carlospavanetti/workordersapi/domain/model/Customer.java
@@ -6,17 +6,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
 @Entity(name = "Customers")
 @Table(name = "Customers")
 public class Customer {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private final Long id;
+  private Long id;
   private String name;
   private String email;
   private String phone;
